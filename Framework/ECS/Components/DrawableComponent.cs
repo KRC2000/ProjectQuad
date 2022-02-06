adaeacc9eb65e255d8a85e329f20abfe261f20b9
Roman Kovalov <pomkov2000@gmail.com>
2022-02-06 22:05:12 +0200

    Added Draw() overload to pass target size

--- Framework/ECS/Components/DrawableComponent.cs
+++ Framework/ECS/Components/DrawableComponent.cs
@@ -12,17 +12,21 @@ namespace Framework.ECS.Components
 		public Texture2D texture;
 		public TransformComponent tc = null;
 		public DrawableComponent() { base.Type = typeof(DrawableComponent); }
+
 		public void Draw(SpriteBatch _batch)
 		{
 			if (Owner.TryGetComponent<TransformComponent>(out tc))
-			{
 				_batch.Draw(texture, new Vector2(tc.X, tc.Y), Color.White);
-			}
 			else
-            {
 				_batch.Draw(texture, new Vector2(0, 0), Color.White);
-            }
-			
+		}
+
+		public void Draw(SpriteBatch _batch, Vector2 size)
+		{
+			if (Owner.TryGetComponent<TransformComponent>(out tc))
+				_batch.Draw(texture, new Rectangle((int)tc.X, (int)tc.Y, (int)size.X, (int)size.Y), Color.White);
+			else
+				_batch.Draw(texture, new Rectangle(0, 0, (int)size.X, (int)size.Y) , Color.White);
 		}
 	}
 }
