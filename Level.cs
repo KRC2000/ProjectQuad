7d306b1c69c324022afe07cac5c0e803ef2d9cff
Roman Kovalov <pomkov2000@gmail.com>
2022-02-20 18:29:30 +0200

    Removed debug calls drawing functionality from Level.cs, due to introduction of DebugUI. Cleaned some mess.

--- Level.cs
+++ Level.cs
@@ -27,7 +27,7 @@ namespace MonogameProj1
         public Texture2D stamp_t = null;
         public SpriteFont font = null;
 
-        public static bool PrintDrawCalls { get; set; }
+        public uint DrawCalls { get; private set; }
 
         public void foo(){
             
@@ -35,29 +35,13 @@ namespace MonogameProj1
 
         public Level(string levelFilePath)
         {
-
-            //FillMapRandomly();
-
             LevelFile = JsonSerializer.Deserialize<LevelFile>(File.ReadAllText(levelFilePath));
         }
-
-        //private void FillMapRandomly()
-        //{
-        //    Random random = new Random();
-        //    for (uint y = 0; y < LevelFile.SizeY; y++)
-        //    {
-        //        for (uint x = 0; x < LevelFile.SizeX; x++)
-        //        {
-        //            LevelFile.Map
-        //            Map[x, y] = Convert.ToBoolean(random.Next(0, 2));
-        //        }
-        //    }
-        //}
 
         public void Draw(SpriteBatch _spriteBatch, Camera camera, GraphicsDevice device, Rectangle viewarea)
         {
             _spriteBatch.Begin(SpriteSortMode.Immediate, null, SamplerState.PointClamp, null, null, null, camera.GetTransform(device.Viewport));
-            uint drawCalls = 0;
+            uint drawCallsCounter = 0;
             for (uint y = 0; y < LevelFile.SizeY; y++)
             {
                 for (uint x = 0; x < LevelFile.SizeX; x++)
@@ -65,20 +49,13 @@ namespace MonogameProj1
                     Vector2 pos = new Vector2(x * Game1.CELLSIZE_X, y * Game1.CELLSIZE_Y);
                     if (viewarea.Contains(pos))
                     {
-                        //if (LevelFile.Map[y * LevelFile.SizeX + x] == 1) _spriteBatch.Draw(stamp_t, pos, Color.White);
                         if (LevelFile.Map[y * LevelFile.SizeX + x] == 1) _spriteBatch.Draw(stamp_t, new Rectangle((int)pos.X, (int)pos.Y, (int)Game1.CELLSIZE_X, (int)Game1.CELLSIZE_Y), Color.White);
-                        drawCalls++;
+                        drawCallsCounter++;
                     }
                 }
             }
             _spriteBatch.End();
-            
-            if (PrintDrawCalls)
-            {
-                _spriteBatch.Begin(SpriteSortMode.Immediate, null, SamplerState.PointClamp, null, null, null, null);
-                _spriteBatch.DrawString(font, $"Level draw calls: {drawCalls}", new Vector2(), Color.White);
-                _spriteBatch.End();
-            }
+            DrawCalls = drawCallsCounter;
         }
     }
 
