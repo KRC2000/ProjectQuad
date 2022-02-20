2bba3c45567a53e42010156e805569072562b1da
Roman Kovalov <pomkov2000@gmail.com>
2022-02-20 11:42:27 +0200

    Cleaned up fields and added comments to GoToComponent. Little additional performance tweaks.

--- Framework/ECS/Components/GoToComponent.cs
+++ Framework/ECS/Components/GoToComponent.cs
@@ -11,42 +11,59 @@ namespace Framework.ECS.Components
 	/// </summary>
     internal class GoToComponent: Base, IUpdatable
     {
-		public float targetX;
-		public float targetY;
+		public bool isTraveling { get; private set; } = false;
 
-		public bool traveling = false;
-		public float traveled;
-		public float initDistance;
+		/// <summary>Distance traveled to the current target</summary>
+		public float Traveled { get; private set; }
 
-		public float speed = 1;
-		public float vx, vy;
-		public float vx_n, vy_n;
+		/// <summary>Whole initial distance between start of travel and finish</summary>
+		public float InitDistance { get; private set; }
+
+		public float Speed { get; set; } = 1;
+
+		public TransformComponent transform_comp {get => tc; private set => tc = value;}
+
+		// Private var's:
+
+		private float targetX;
+		private float targetY;
+
+		/// <summary>Start to finish vector</summary>
+		private float vx, vy;
+
+		/// <summary>Start to finish vector normalised</summary>
+		private float vx_n, vy_n;
 		
-		TransformComponent tc = null;
-
+		private TransformComponent tc = null; 
+		
+		/// <summary>
+		/// Moves transform component of the owner to the passed coordinate<br></br>
+		/// Update() should be called in cycle to work
+		/// </summary>
 		public void GoTo(float x, float y)
 		{
-			if (Owner.TryGetComponent<TransformComponent>(out tc))
+			if (Owner.TryGetComponent<TransformComponent>(out tc) ||
+				tc != null)
 			{
 				targetX = x;
 				targetY = y;
 
 				vx = targetX - tc.X; vy = targetY - tc.Y;
-				initDistance = (float)Math.Sqrt(vx * vx + vy * vy);
-				vx_n = vx / initDistance;
-				vy_n = vy / initDistance;
+				InitDistance = (float)Math.Sqrt(vx * vx + vy * vy);
+				vx_n = vx / InitDistance;
+				vy_n = vy / InitDistance;
 
-				traveling = true;
+				isTraveling = true;
 			}
 			else throw new Exception($"Entity does not has required component for system correct execution. Missing component - {typeof(TransformComponent)}");
 		}
 
         public void Update()
         {
-			if (traveling){
-				tc.Move(vx_n * speed, vy_n * speed);
-				traveled += 1 * speed;
-				if (traveled >= initDistance) traveling = false;
+			if (isTraveling){
+				tc.Move(vx_n * Speed, vy_n * Speed);
+				Traveled += 1 * Speed;
+				if (Traveled >= InitDistance) isTraveling = false;
             }
         }
 
