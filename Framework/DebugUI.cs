1d05e5dac1de3ba8bb69529be6c63c5929fa305a
Roman <pomkov2000@gmail.com>
2022-04-01 18:37:44 +0300

    Minor fixes, exception handling

--- Framework/DebugUI.cs
+++ Framework/DebugUI.cs
@@ -10,8 +10,10 @@ namespace Framework.DebugUI
     {
         public static SpriteFont Font { get; set; }
 
-        static public void DrawDebug<T>(SpriteBatch batch, string message, in T var, uint order)
+        static public void DrawDebug<T>(SpriteBatch batch, string message, T var, uint order)
         {
+            if (Font == null) throw new Exception("Error:Framework.DebugUI: No font was set"); 
+            
             batch.Begin(SpriteSortMode.Immediate, null, SamplerState.PointClamp);
             batch.DrawString(Font, string.Concat(message, var.ToString()), new Vector2(0, order * Font.MeasureString("Q").Y), Color.White);
             batch.End();
