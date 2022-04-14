d6dedcb3b416db74f4c4467c3aed161b11aa3005
Roman Kovalov <pomkov2000@gmail.com>
2022-04-14 18:38:55 +0300

    Added resources.

---
+++ Content/classicTileset2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="classicTileset2" tilewidth="20" tileheight="20" tilecount="160" columns="16">
+ <image source="tileset2.png" width="320" height="200"/>
+</tileset>
