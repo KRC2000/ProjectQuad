820b7a93a068f301a6f714f68ce0dc87ff248160
Roman Kovalov <pomkov2000@gmail.com>
2022-04-16 15:15:13 +0300

    Fixed bug when ordering go on the occupied cell when already traveling caused crash Changed formating - now indentation is made with tabs.

--- Level.cs
+++ Level.cs
@@ -12,203 +12,203 @@ using Framework.Camera;
 
 namespace ProjectQuad
 {
-    class Level
-    {
-        public LevelFile LevelFile { get; set; }
+	class Level
+	{
+		public LevelFile LevelFile { get; set; }
 
-        public Dictionary<string, Tileset> Tilesets { get; private set; }
+		public Dictionary<string, Tileset> Tilesets { get; private set; }
 
-        public Layer ObstacleLayer { get; private set; }
-        public string Name { get; private set; } = null;
+		public Layer ObstacleLayer { get; private set; }
+		public string Name { get; private set; } = null;
 
-        private Texture2D stamp_t = null;
-        public SpriteFont font = null;
+		private Texture2D stamp_t = null;
+		public SpriteFont font = null;
 
-        public uint DrawCalls { get; private set; }
+		public uint DrawCalls { get; private set; }
 
-        public Level(string levelFilePath, string levelName)
-        {
-            // Deserialise level xml file in to LevelFile
-            using (StreamReader reader = new StreamReader(levelFilePath))
-            {
-                XmlSerializer serializer = new XmlSerializer(typeof(LevelFile));
-                LevelFile = (LevelFile)serializer.Deserialize(reader);
+		public Level(string levelFilePath, string levelName)
+		{
+			// Deserialise level xml file in to LevelFile
+			using (StreamReader reader = new StreamReader(levelFilePath))
+			{
+				XmlSerializer serializer = new XmlSerializer(typeof(LevelFile));
+				LevelFile = (LevelFile)serializer.Deserialize(reader);
 
-                Tilesets = new Dictionary<string, Tileset>();
-                
-                foreach (var tileset_ref in LevelFile.Tileset_refs)
-                {
+				Tilesets = new Dictionary<string, Tileset>();
 
-                    using (StreamReader reader1 = new StreamReader(Path.Join("Content", tileset_ref.Source_tsx)))
-                    {
-                        XmlSerializer serializer1 = new XmlSerializer(typeof(Tileset));
-                        Tileset newts = (Tileset)serializer1.Deserialize(reader1);
-                        newts.this_ref = tileset_ref;
-                        Tilesets.Add(newts.Name, newts);
-                    }
-                }
-            }
+				foreach (var tileset_ref in LevelFile.Tileset_refs)
+				{
 
-            // Map data can be only read as a string, so
-            // converting long ass string ("..0, 0, 0, 1, 1, 0,\n..") into 2D array that Layer objects will own.
-            // Also assigning obstacle layer var
-            foreach (var layer in LevelFile.Layers)
-            {
-                List<string> rows = new List<string>();
-                rows.AddRange(layer.Data_str.Split("\n"));
-                rows.RemoveAt(0);
-                rows.RemoveAt(rows.Count - 1);
+					using (StreamReader reader1 = new StreamReader(Path.Join("Content", tileset_ref.Source_tsx)))
+					{
+						XmlSerializer serializer1 = new XmlSerializer(typeof(Tileset));
+						Tileset newts = (Tileset)serializer1.Deserialize(reader1);
+						newts.this_ref = tileset_ref;
+						Tilesets.Add(newts.Name, newts);
+					}
+				}
+			}
 
-                layer.Data = new uint[layer.Height, layer.Width];
-                for (int i = 0; i < rows.Count; i++)
-                {
-                    List<string> row = new List<string>();
-                    row.AddRange(rows[i].Split(","));
-                    row.RemoveAt(row.Count - 1);
+			// Map data can be only read as a string, so
+			// converting long ass string ("..0, 0, 0, 1, 1, 0,\n..") into 2D array that Layer objects will own.
+			// Also assigning obstacle layer var
+			foreach (var layer in LevelFile.Layers)
+			{
+				List<string> rows = new List<string>();
+				rows.AddRange(layer.Data_str.Split("\n"));
+				rows.RemoveAt(0);
+				rows.RemoveAt(rows.Count - 1);
 
-                    for (int j = 0; j < row.Count; j++)
-                    {
-                        layer.Data[i, j] = uint.Parse(row[j]);
-                    }
-                }
+				layer.Data = new uint[layer.Height, layer.Width];
+				for (int i = 0; i < rows.Count; i++)
+				{
+					List<string> row = new List<string>();
+					row.AddRange(rows[i].Split(","));
+					row.RemoveAt(row.Count - 1);
 
-                if (layer.Name == "Obstacles") ObstacleLayer = layer;
-            }
-        }
+					for (int j = 0; j < row.Count; j++)
+					{
+						layer.Data[i, j] = uint.Parse(row[j]);
+					}
+				}
 
-        public void LoadTilesets(ContentManager content)
-        {
-            foreach (var tileset in Tilesets)
-            {
-                tileset.Value.Image.Texture = content.Load<Texture2D>(Path.GetFileNameWithoutExtension(tileset.Value.Image.Source_png));
-            }
-        }
+				if (layer.Name == "Obstacles") ObstacleLayer = layer;
+			}
+		}
 
-        public void Draw(SpriteBatch _spriteBatch, Camera camera, GraphicsDevice device, Rectangle viewarea)
-        {
-            _spriteBatch.Begin(SpriteSortMode.Deferred, null, SamplerState.PointClamp, null, null, null, camera.GetTransform(device.Viewport));
-            uint drawCallsCounter = 0;
+		public void LoadTilesets(ContentManager content)
+		{
+			foreach (var tileset in Tilesets)
+			{
+				tileset.Value.Image.Texture = content.Load<Texture2D>(Path.GetFileNameWithoutExtension(tileset.Value.Image.Source_png));
+			}
+		}
 
-            foreach (var layer in LevelFile.Layers)
-            {
-                for (int y = 0; y < layer.Width; y++)
-                {
-                    for (int x = 0; x < layer.Height; x++)
-                    {
-                        Vector2 pos = new Vector2(y * Game1.CELLSIZE_X, x * Game1.CELLSIZE_Y);
+		public void Draw(SpriteBatch _spriteBatch, Camera camera, GraphicsDevice device, Rectangle viewarea)
+		{
+			_spriteBatch.Begin(SpriteSortMode.Deferred, null, SamplerState.PointClamp, null, null, null, camera.GetTransform(device.Viewport));
+			uint drawCallsCounter = 0;
 
-                        if (viewarea.Contains(pos))
-                        {
-                            uint tileValue = layer.Data[x, y];
-                            Rectangle sourceRect = new Rectangle();
-                            if (tileValue != 0)
-                            {
-                                foreach (var tileset in Tilesets)
-                                {
-                                    // if tile value is in the tile range of current tileset -> pick and assign texture used by this tileset
-                                    if (tileValue > tileset.Value.this_ref.FirstElementId && tileValue < tileset.Value.this_ref.FirstElementId + tileset.Value.TileCount)
-                                    {
-                                        stamp_t = tileset.Value.Image.Texture;
+			foreach (var layer in LevelFile.Layers)
+			{
+				for (int y = 0; y < layer.Width; y++)
+				{
+					for (int x = 0; x < layer.Height; x++)
+					{
+						Vector2 pos = new Vector2(y * Game1.CELLSIZE_X, x * Game1.CELLSIZE_Y);
 
-                                        int r_x = (int)(tileValue - tileset.Value.this_ref.FirstElementId) % tileset.Value.Columns;
-                                        int r_y = (int)(tileValue - tileset.Value.this_ref.FirstElementId) / tileset.Value.Columns;
-                                        sourceRect = new Rectangle(r_x * tileset.Value.TileWidth, r_y * tileset.Value.TileHeight, tileset.Value.TileWidth, tileset.Value.TileHeight);
-                                    }
-                                }
+						if (viewarea.Contains(pos))
+						{
+							uint tileValue = layer.Data[x, y];
+							Rectangle sourceRect = new Rectangle();
+							if (tileValue != 0)
+							{
+								foreach (var tileset in Tilesets)
+								{
+									// if tile value is in the tile range of current tileset -> pick and assign texture used by this tileset
+									if (tileValue > tileset.Value.this_ref.FirstElementId && tileValue < tileset.Value.this_ref.FirstElementId + tileset.Value.TileCount)
+									{
+										stamp_t = tileset.Value.Image.Texture;
 
-                                _spriteBatch.Draw(stamp_t, new Rectangle((int)pos.X, (int)pos.Y, (int)Game1.CELLSIZE_X, (int)Game1.CELLSIZE_Y), sourceRect, Color.White, 0, new Vector2(0, 0), SpriteEffects.None, 0);
-                                drawCallsCounter++;
-                            }
-                        }
-                    }
-                }
-            }
+										int r_x = (int)(tileValue - tileset.Value.this_ref.FirstElementId) % tileset.Value.Columns;
+										int r_y = (int)(tileValue - tileset.Value.this_ref.FirstElementId) / tileset.Value.Columns;
+										sourceRect = new Rectangle(r_x * tileset.Value.TileWidth, r_y * tileset.Value.TileHeight, tileset.Value.TileWidth, tileset.Value.TileHeight);
+									}
+								}
+
+								_spriteBatch.Draw(stamp_t, new Rectangle((int)pos.X, (int)pos.Y, (int)Game1.CELLSIZE_X, (int)Game1.CELLSIZE_Y), sourceRect, Color.White, 0, new Vector2(0, 0), SpriteEffects.None, 0);
+								drawCallsCounter++;
+							}
+						}
+					}
+				}
+			}
 
 
-            _spriteBatch.End();
-            DrawCalls = drawCallsCounter;
-        }
+			_spriteBatch.End();
+			DrawCalls = drawCallsCounter;
+		}
 
-        public bool IsPassable(Point tilePos)
-        {
-            if (ObstacleLayer.Data[tilePos.Y, tilePos.X] == 0) return true;
-            else return false;
-        }
-    }
+		public bool IsPassable(Point tilePos)
+		{
+			if (ObstacleLayer.Data[tilePos.Y, tilePos.X] == 0) return true;
+			else return false;
+		}
+	}
 
-    // These are deserialisation classes
+	// These are deserialisation classes
 
-    [XmlRoot("map")]
-    public class LevelFile
-    {
-        [XmlElement("tileset")]
-        public Tileset_ref[] Tileset_refs { get; set; }
+	[XmlRoot("map")]
+	public class LevelFile
+	{
+		[XmlElement("tileset")]
+		public Tileset_ref[] Tileset_refs { get; set; }
 
-        [XmlElement("layer")]
-        public Layer[] Layers { get; set; }
-    }
+		[XmlElement("layer")]
+		public Layer[] Layers { get; set; }
+	}
 
-    public class Layer
-    {
-        [XmlAttribute("id")]
-        public int Id { get; set; }
-        [XmlAttribute("name")]
-        public string Name { get; set; }
-        [XmlAttribute("width")]
-        public int Width { get; set; }
-        [XmlAttribute("height")]
-        public int Height { get; set; }
-        [XmlElement("data")]
-        public string Data_str { get; set; }
-        [XmlIgnore]
-        public uint[,] Data { get; set; }
-    }
-    public class Tileset_ref
-    {
-        [XmlAttribute("firstgid")]
-        public int FirstElementId { get; set; }
-        [XmlAttribute("source")]
-        public string Source_tsx { get; set; }
-    }
+	public class Layer
+	{
+		[XmlAttribute("id")]
+		public int Id { get; set; }
+		[XmlAttribute("name")]
+		public string Name { get; set; }
+		[XmlAttribute("width")]
+		public int Width { get; set; }
+		[XmlAttribute("height")]
+		public int Height { get; set; }
+		[XmlElement("data")]
+		public string Data_str { get; set; }
+		[XmlIgnore]
+		public uint[,] Data { get; set; }
+	}
+	public class Tileset_ref
+	{
+		[XmlAttribute("firstgid")]
+		public int FirstElementId { get; set; }
+		[XmlAttribute("source")]
+		public string Source_tsx { get; set; }
+	}
 
-    [XmlRoot("tileset")]
-    public class Tileset
-    {
-        [XmlAttribute("name")]
-        public string Name { get; set; }
+	[XmlRoot("tileset")]
+	public class Tileset
+	{
+		[XmlAttribute("name")]
+		public string Name { get; set; }
 
-        [XmlAttribute("tilewidth")]
-        public int TileWidth { get; set; }
+		[XmlAttribute("tilewidth")]
+		public int TileWidth { get; set; }
 
-        [XmlAttribute("tileheight")]
-        public int TileHeight { get; set; }
+		[XmlAttribute("tileheight")]
+		public int TileHeight { get; set; }
 
-        [XmlAttribute("tilecount")]
-        public int TileCount { get; set; }
+		[XmlAttribute("tilecount")]
+		public int TileCount { get; set; }
 
-        [XmlAttribute("columns")]
-        public int Columns { get; set; }
+		[XmlAttribute("columns")]
+		public int Columns { get; set; }
 
-        [XmlElement("image")]
-        public Image Image { get; set; }
+		[XmlElement("image")]
+		public Image Image { get; set; }
 
-        // Reference to this tileset from map file
-        [XmlIgnore]
-        public Tileset_ref this_ref { get; set; }
-    }
+		// Reference to this tileset from map file
+		[XmlIgnore]
+		public Tileset_ref this_ref { get; set; }
+	}
 
-    public class Image
-    {
-        [XmlAttribute("source")]
-        public string Source_png { get; set; }
+	public class Image
+	{
+		[XmlAttribute("source")]
+		public string Source_png { get; set; }
 
-        [XmlAttribute("width")]
-        public int Width { get; set; }
+		[XmlAttribute("width")]
+		public int Width { get; set; }
 
-        [XmlAttribute("height")]
-        public int height { get; set; }
+		[XmlAttribute("height")]
+		public int height { get; set; }
 
-        [XmlIgnore]
-        public Texture2D Texture { get; set; }
-    }
+		[XmlIgnore]
+		public Texture2D Texture { get; set; }
+	}
 }
--- LocalFramwork/TravelComponent.cs
+++ LocalFramwork/TravelComponent.cs
@@ -9,116 +9,124 @@ using Framework.Pathfinder;
 
 namespace ProjectQuad.Framework.Components
 {
-    /// <summary>
-    /// Requires:<br></br>
-    /// Framework.ECS.Components.TransformComponent;<br></br>
-    /// Framework.ECS.Components.GoToComponent;
-    /// </summary>
-    class TravelComponent : Base
-    {
-        private GoToComponent gtc = null;
-        private TransformComponent tc = null;
-        private Pathfinder finder = null;
+	/// <summary>
+	/// Requires:<br></br>
+	/// Framework.ECS.Components.TransformComponent;<br></br>
+	/// Framework.ECS.Components.GoToComponent;
+	/// </summary>
+	class TravelComponent : Base
+	{
+		private GoToComponent gtc = null;
+		private TransformComponent tc = null;
+		private Pathfinder finder = null;
 
 
-        public TravelComponent() { base.Type = typeof(TravelComponent); }
+		public TravelComponent() { base.Type = typeof(TravelComponent); }
 
-        public void TravelOneStep(Direction direction, Level currentLevel)
-        {
-            VerifyRequiredComponents();
+		public void TravelOneStep(Direction direction, Level currentLevel)
+		{
+			VerifyRequiredComponents();
 
-            Point gridPos = new Point((int)((tc.Pos.X + Game1.CELLSIZE_X / 2) / Game1.CELLSIZE_X),
-                                        (int)((tc.Pos.Y + Game1.CELLSIZE_Y / 2) / Game1.CELLSIZE_Y));
+			Point gridPos = new Point((int)((tc.Pos.X + Game1.CELLSIZE_X / 2) / Game1.CELLSIZE_X),
+										(int)((tc.Pos.Y + Game1.CELLSIZE_Y / 2) / Game1.CELLSIZE_Y));
 
-                switch (direction)
-                {
-                    case Direction.N:
-                        if (currentLevel.IsPassable(new Point(gridPos.X, gridPos.Y - 1))) {
-                            gtc.GoTo(new Vector2(tc.Pos.X, tc.Pos.Y - Game1.CELLSIZE_Y));
-                        }
-                    break;
-                    case Direction.W:
-                        if (currentLevel.IsPassable(new Point(gridPos.X - 1, gridPos.Y))) {
-                            gtc.GoTo(new Vector2(tc.Pos.X - Game1.CELLSIZE_X, tc.Pos.Y));
-                        }
-                    break;
-                    case Direction.S:
-                        if (currentLevel.IsPassable(new Point(gridPos.X, gridPos.Y + 1))) {
-                            gtc.GoTo(new Vector2(tc.Pos.X, tc.Pos.Y + Game1.CELLSIZE_Y));
-                        }
-                    break;
-                    case Direction.E:
-                        if (currentLevel.IsPassable(new Point(gridPos.X + 1, gridPos.Y))) {
-                            gtc.GoTo(new Vector2(tc.Pos.X + Game1.CELLSIZE_X, tc.Pos.Y));
-                        }
-                    break;
-                }
-        }
-        
-        /// <summary>
-        /// Depending on the passed bool queue variable, either makes entity travel to the cellPos, or queues such travel in the end of the already queued movement.
-        /// Travel path accounts level and obstacles, shortest path calculated.
-        /// </summary>
-        /// <param name="cellPos">Travel final destination</param>
-        /// <param name="currentLevel">Level that entity is traveling on</param>
-        /// <param name="queue">Should travel be queued(true) or started immediatly(false)</param>
-        public void TravelTo(Point cellPos, Level currentLevel, bool queue)
-        {
-            VerifyRequiredComponents();
+			switch (direction)
+			{
+				case Direction.N:
+					if (currentLevel.IsPassable(new Point(gridPos.X, gridPos.Y - 1)))
+					{
+						gtc.GoTo(new Vector2(tc.Pos.X, tc.Pos.Y - Game1.CELLSIZE_Y));
+					}
+					break;
+				case Direction.W:
+					if (currentLevel.IsPassable(new Point(gridPos.X - 1, gridPos.Y)))
+					{
+						gtc.GoTo(new Vector2(tc.Pos.X - Game1.CELLSIZE_X, tc.Pos.Y));
+					}
+					break;
+				case Direction.S:
+					if (currentLevel.IsPassable(new Point(gridPos.X, gridPos.Y + 1)))
+					{
+						gtc.GoTo(new Vector2(tc.Pos.X, tc.Pos.Y + Game1.CELLSIZE_Y));
+					}
+					break;
+				case Direction.E:
+					if (currentLevel.IsPassable(new Point(gridPos.X + 1, gridPos.Y)))
+					{
+						gtc.GoTo(new Vector2(tc.Pos.X + Game1.CELLSIZE_X, tc.Pos.Y));
+					}
+					break;
+			}
+		}
 
-            if (finder == null) {
-                finder = new Pathfinder();
-                finder.Quiet = true;
-                finder.Mode = PathMode.Aligned;
-            }
+		/// <summary>
+		/// Depending on the passed bool queue variable, either makes entity travel to the cellPos, or queues such travel in the end of the already queued movement.
+		/// Travel path accounts level and obstacles, shortest path calculated.
+		/// </summary>
+		/// <param name="cellPos">Travel final destination</param>
+		/// <param name="currentLevel">Level that entity is traveling on</param>
+		/// <param name="queue">Should travel be queued(true) or started immediatly(false)</param>
+		public void TravelTo(Point cellPos, Level currentLevel, bool queue)
+		{
+			VerifyRequiredComponents();
 
-            List<Point> path = null;
-            if (!finder.Initialised || finder.Tag != currentLevel.Name)
-            {
-                path = new List<Point>();
-                finder.Init(currentLevel.ObstacleLayer.Data, currentLevel.Name);
-            }
+			// Set up pathfinder object if it wasn't done
+			if (finder == null)
+			{
+				finder = new Pathfinder();
+				finder.Quiet = true;
+				finder.Mode = PathMode.Aligned;
+			}
 
-            
-            Point pos = new Point((int)((tc.Pos.X + Game1.CELLSIZE_X / 2) / Game1.CELLSIZE_X),
-                                    (int)((tc.Pos.Y + Game1.CELLSIZE_Y / 2) / Game1.CELLSIZE_Y));
+			// Create path container, if pathfinder not initialised with this level - initialise it.
+			List<Point> path = null;
+			if (!finder.Initialised || finder.Tag != currentLevel.Name)
+				finder.Init(currentLevel.ObstacleLayer.Data, currentLevel.Name);
 
-            if (queue && gtc.Queue.Count > 0)
-            {
-                finder.GetPath(out path, new Point((int)(gtc.Queue[gtc.Queue.Count-1].X / Game1.CELLSIZE_X), 
-                                                    (int)(gtc.Queue[gtc.Queue.Count-1].Y / Game1.CELLSIZE_Y)), cellPos);
-            }
-            else
-            {
-                finder.GetPath(out path, pos, cellPos);
-                gtc.Queue.Clear();
-            }
-            
-            if (path != null)
-            {
-                foreach (var point in path)
-                {
-                    gtc.AddToQueue(new Vector2(point.X * Game1.CELLSIZE_X, point.Y * Game1.CELLSIZE_Y));
-                }
-            }
-        }
+			// Get entity grid position
+			Point pos = new Point((int)((tc.Pos.X + Game1.CELLSIZE_X / 2) / Game1.CELLSIZE_X),
+									(int)((tc.Pos.Y + Game1.CELLSIZE_Y / 2) / Game1.CELLSIZE_Y));
 
-        private void VerifyRequiredComponents()
-        {
-            if (tc == null){
+			// if (queue == true) then find a path starting from last position in the queue,
+			// otherwise find path from the current entity position and clear the queue
+			if (queue && gtc.Queue.Count > 0)
+			{
+				finder.GetPath(out path, new Point((int)(gtc.Queue[gtc.Queue.Count - 1].X / Game1.CELLSIZE_X),
+													(int)(gtc.Queue[gtc.Queue.Count - 1].Y / Game1.CELLSIZE_Y)), cellPos);
+			}
+			else
+			{
+				finder.GetPath(out path, pos, cellPos);
+				if (path != null) gtc.Queue.Clear();
+			}
+
+			if (path != null)
+			{
+				foreach (var point in path)
+				{
+					gtc.AddToQueue(new Vector2(point.X * Game1.CELLSIZE_X, point.Y * Game1.CELLSIZE_Y));
+				}
+			}
+		}
+
+		private void VerifyRequiredComponents()
+		{
+			if (tc == null)
+			{
 				if (!Owner.TryGetComponent<TransformComponent>(out tc))
-                	throw new Exception($"{this.GetType()}: Entity does not has required component for system correct execution. Missing component - {typeof(TransformComponent)}");
+					throw new Exception($"{this.GetType()}: Entity does not has required component for system correct execution. Missing component - {typeof(TransformComponent)}");
 			}
-            if (gtc == null){
+			if (gtc == null)
+			{
 				if (!Owner.TryGetComponent<GoToComponent>(out gtc))
-                	throw new Exception($"{this.GetType()}: Entity does not has required component for system correct execution. Missing component - {typeof(GoToComponent)}");
+					throw new Exception($"{this.GetType()}: Entity does not has required component for system correct execution. Missing component - {typeof(GoToComponent)}");
 			}
-        }
+		}
 
-        public enum Direction
-        {
-            N, E, S, W,
-            NE, NW, SE, SW
-        }
-    }
+		public enum Direction
+		{
+			N, E, S, W,
+			NE, NW, SE, SW
+		}
+	}
 }
